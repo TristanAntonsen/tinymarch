24c55e3cedbd8e4d4b300d9883a283cee1c3e17e
TristanAntonsen <tristan.antonsen@gmail.com>
2023-05-18 18:46:22 -0500

    basic ray marching

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use image::io::Reader as ImageReader;
 use image::{DynamicImage, GenericImageView, ImageBuffer, Rgb, RgbImage, Pixel};
-use nalgebra::{vector, Point3, Vector3, point};
+use nalgebra::{vector, Point3, Vector3, point, distance};
 
 // ======================================================================
 // ======================= Data types & Constants =======================
@@ -10,6 +10,10 @@ pub type Point = Point3<f64>;
 pub type Vector = Vector3<f64>;
 pub type Color = Vector3<f64>;
 
+pub const MAX_STEPS: usize =  100;
+pub const MAX_DIST: f64 =  100. ;
+pub const SURF_DIST: f64 =  0.01;
+
 pub const BLACK: Color = vector![0.0, 0.0, 0.0];
 pub const WHITE: Color = vector![1.0, 1.0, 1.0];
 pub const RED: Color = vector![1.0, 0.0, 0.0];
@@ -18,7 +22,7 @@ pub const BLUE: Color = vector![0.0, 0.0, 1.0];
 
 pub fn render(res_x: usize, res_y: usize) {
 
-    let ro = point![0., 0., 0.];
+    let ro = point![0., 0., -1.];
     let viewport_width = 1.0;
     let viewport_height = res_y as f64 / res_x as f64;    
 
@@ -35,12 +39,23 @@ pub fn render(res_x: usize, res_y: usize) {
                 .into_iter()
                 .map(|y| {
 
+                    // screen space coordinates
                     let u = x as f64 / res_x as f64;
                     let v = y as f64 / res_y as f64;
-                    
+
+                    // ray direction
                     let rd = (lower_left_corner + u*horizontal + v*vertical - ro).normalize();
-                    let t = 0.5*(rd.y + 1.0);
-                    (1.0-t) * vec3(1.0) + t * vector!(0.5, 0.7, 1.0)
+
+                    // ray marching
+                    let d = ray_march(ro, rd);
+
+                    // shading
+                    if d >= MAX_DIST {
+                        let t = 0.5*(rd.y + 1.0);
+                        (1.0-t) * vector![1., 1., 1.] + t * vector!(0.5, 0.7, 1.0)
+                    } else {
+                        return RED; 
+                    }
                 })
                 .collect::<Vec<Color>>()
         })
@@ -49,8 +64,22 @@ pub fn render(res_x: usize, res_y: usize) {
     save_png(pixels, "output.png")
 }
 
-pub fn vec3(a: f64) -> Vector {
-    vector![a, a, a]
+pub fn eval(p: Point) -> f64 {
+    return distance(&p, &point![0.0, 0.0, 1.0]) - 0.5
+}
+
+pub fn ray_march(ro: Point, rd: Vector) -> f64 {
+    let mut d = 0.0;
+
+    for _ in 0..MAX_STEPS {
+        let p = ro + rd * d;
+        let ds = eval(p);
+        d += ds;
+        if d >= MAX_DIST || ds < SURF_DIST {
+            return d
+        }
+    }
+    return d
 }
 
 pub fn save_png(pixels: Vec<Vec<Color>>, path: &str) {
