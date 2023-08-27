c624b90fa5217f988f114c40e1ff1576ca4372d3
TristanAntonsen <tristan.antonsen@gmail.com>
2023-08-27 09:31:18 -0500

    add two primitives

--- src/lib.rs
+++ src/lib.rs
@@ -176,6 +176,18 @@ fn _rounded_box(p: Point, s: Vector, r: f64) -> f64 {
     return vector![pf.x.max(0.0),pf.y.max(0.0),pf.z.max(0.0)].norm() + pf.x.max(pf.y.max(pf.z)).min(0.0) - r;
 }
 
+fn _rounded_cylinder(p: Point, r1: f64, r2: f64, h: f64) -> f64 {
+    let d = vector![vector![p.x, p.z].norm()-2.0*r1+r2, p.y.abs() - h];
+    return d.x.max(d.y).min(0.0) + vector![d.x.max(0.0), d.y.max(0.0)].norm() - r2
+}
+
+fn _torus( p: Point, r1: f64, r2: f64 ) -> f64 {
+// vec2 q = vec2(length(p.xz)-t.x,p.y);
+// return length(q)-t.y;
+  let q = vector![vector![p.x, p.z].norm()-r1, p.y];
+  return q.norm()-r2;
+}
+
 ////////////////////////////////////////////////////////////////
 // SDF Operations
 ////////////////////////////////////////////////////////////////
